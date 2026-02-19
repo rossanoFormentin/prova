3abac35b8321f793a5421b22c26ddb34e2bbdd7d
rossanoFormentin <108268287+rossanoFormentin@users.noreply.github.com>
2026-02-19 14:19:44 +0100

    6

--- js/calendario.js
+++ js/calendario.js
@@ -219,14 +219,19 @@ async function openDayModal(date, event=null){
     const result = await Swal.fire({
         title: `Giorno ${date}`,
         html: `
-            <div>Stato:</div><select id="status" class="swal2-input">
-                <option value="presenza">Presenza</option>
-                <option value="smart">Smart Working</option>
-                <option value="ferie">Ferie</option>
-                <option value="festivita">Festività</option>
-                <option value="supplementare">Supplementare</option>
-                <option value="scoperto">Scoperto</option>
-            </select>
+            <div class="form-row">
+    <div class="form-label">Stato:</div>
+    <div class="form-input">
+        <select id="status" class="swal2-input">
+            <option value="presenza">Presenza</option>
+            <option value="smart">Smart Working</option>
+            <option value="ferie">Ferie</option>
+            <option value="festivita">Festività</option>
+            <option value="supplementare">Supplementare</option>
+            <option value="scoperto">Scoperto</option>
+        </select>
+    </div>
+</div>
             <input id="note" class="swal2-input" placeholder="Note">
             <label style="margin-top:5px">
                 <input type="checkbox" id="giustificativo"> Giustificativo
